694795a3e1220802c455c97399fed90501e9e5cb
somoriiin <29141720+somoriiin@users.noreply.github.com>
2023-12-23 03:36:54 +0900

    add the function of reading and printing cert

--- main.go
+++ main.go
@@ -2,7 +2,10 @@
 package main
 
 import (
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -10,8 +13,23 @@ func main() {
 
 	data, err := os.ReadFile("testdata/server.crt")
 	if err != nil {
-		fmt.Println("error")
+		log.Fatalf("Error %v", err)
 	}
 	fmt.Println(string(data))
 
+	bl, _ := pem.Decode(data)
+	if bl == nil {
+		log.Fatalf("Failed to decode PEM block")
+	}
+
+	cert, err := x509.ParseCertificate(bl.Bytes)
+	if err != nil {
+		log.Fatalf("Error %v", err)
+	}
+
+	fmt.Println("Subject: ", cert.Subject.String())
+	fmt.Println("Issuer: ", cert.Issuer.String())
+	fmt.Println("ValidFrom: ", cert.NotBefore.String())
+	fmt.Println("ValidUntil: ", cert.NotAfter.String())
+	fmt.Println("PublicKey: ", fmt.Sprintf("%v", cert.PublicKey))
 }
