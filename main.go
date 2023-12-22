153ea313402289c6ae198faa574bf80695caeca1
Kiyo <29141720+somoriiin@users.noreply.github.com>
2023-12-23 03:39:23 +0900

    Add Dev (#3)
    * add cert testdata and reading file function
    
    * add the function of reading and printing cert

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 // main
 package main
 
-import "fmt"
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Cert")
+
+	data, err := os.ReadFile("testdata/server.crt")
+	if err != nil {
+		log.Fatalf("Error %v", err)
+	}
+	fmt.Println(string(data))
+
+	bl, _ := pem.Decode(data)
+	if bl == nil {
+		log.Fatalf("Failed to decode PEM block")
+	}
+
+	cert, err := x509.ParseCertificate(bl.Bytes)
+	if err != nil {
+		log.Fatalf("Error %v", err)
+	}
+
+	fmt.Println("Subject: ", cert.Subject.String())
+	fmt.Println("Issuer: ", cert.Issuer.String())
+	fmt.Println("ValidFrom: ", cert.NotBefore.String())
+	fmt.Println("ValidUntil: ", cert.NotAfter.String())
+	fmt.Println("PublicKey: ", fmt.Sprintf("%v", cert.PublicKey))
 }
